90e44e21c607c202bc3809b3558eeeacaf43a616
Darioantonio20 <antoniodario10@hotmail.com>
2023-01-18 23:19:55 -0600

    segundo commint con  mejoras

---
+++ src/components/atoms/Header.jsx
@@ -0,0 +1,14 @@
+import '../../assets/imgs/styles/Header.css'
+
+function Header() {
+    return ( 
+        <header>
+            <div className="headerVerde">   
+                <h1>Darío Antonio Gutiérrez Álvarez</h1>
+                <h2>Engineering Student in Software Development</h2>
+            </div>
+        </header>
+     );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import Header from "../components/atoms/Header";
+import '../assets/imgs/styles/Header.css'
+
+function Home() {
+    return ( 
+        <>
+            <Header></Header>
+        </>
+     );
+}
+
+export default Home;
